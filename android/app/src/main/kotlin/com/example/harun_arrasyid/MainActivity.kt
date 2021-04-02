53f46db1d7f45ccb411e29b0548324703fb1d38f
Nahl <nahlultra@gmail.com>
2021-04-02 21:23:14 +0700

    yey

---
+++ android/app/src/main/kotlin/com/example/harun_arrasyid/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.harun_arrasyid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/home.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: DefaultTextStyle(
+          style: GoogleFonts.workSans(color: Colors.black),
+          child: ListView(
+            padding: EdgeInsets.only(right: 24, left: 24, top: 64, bottom: 0),
+            children: [
+              Text("Harun Ar-Rasyid",
+                  style: GoogleFonts.workSans(
+                      fontWeight: FontWeight.bold, fontSize: 42)),
+              Text("Biografi", style: TextStyle(fontSize: 24)),
+              SizedBox(height: 64),
+              Text(
+                  '''Salah satu tokoh besar umat ini yang berhasil membuat Romawi menundukkan kepala karena wibawanya adalah al-Khalifah al-Mujahid Harun al-Rasyid rahimahullah. Seorang laki-laki mulia yang dikaburkan sejarahnya dan dibunuh karakternya oleh orang-orang yang membenci Islam dan kaum muslimin. Ia digambarkan sebagai seorang pemabuk yang gila. Laki-laki hidung belang dengan banyak selir. Pemimpin kejam dan zalim. Padahal dia adalah khalifah terbaik di Daulah Abbasiyah. Ia seorang mujahid. Pemimpin yang perhatian terhadap ilmu dan ulama. Dan keutamaan lainnya. Mungkin inilah yang menyebabkan fitnah itu dihembuskan. Ia digambarkan sebagai pemimpin yang tak bertanggung jawab. Di sampingnya hanya ada khamr dan mabuk. Dibuatlah kisah-kisah palsu dan hikayat-hikayat dusta untuk mendukung fitnah itu.
+
+Ibnu Khalkan berkata, “Harun al-Rasyid termasuk khalifah yang paling mulia dan raya yang paling melayani. Ia berhaji, berjihad, berperang, pemberani, dan cerdas.” (Siyar A’lam Nubala, Juz: 7 al-Rasyid).
+
+Nasab dan Kelahirannya
+
+Kun-yahnya adalah Abu Ja’far. Sedangkan nama dan nasabnya adalah Harun bin al-Mahdi Muhammad bin al-Manshur Abu Ja’far Abdullah bin Muhammad bin Ali bin Abdullah bin Abbas al-Qurasyi al-Hasyimi al-Abbasi. Jadi, ia adalah seorang Quraisy satu kabilah dengan Nabi Muhammad ﷺ. Dan keturunan dari paman Nabi, Abbas bin Abdul Muthalib radhiallahu ‘anhu.
+
+Harun al-Rasyid dilahirkan pada tahun 148 H di Kota Ray. Kala itu, ayahnya menjadi pemimpin wilayah Ray dan Khurasan. Ibunya adalah al-Khayziran (Arab: الخيزران), kun-yahnya Ummul Hadi.
+
+Sejak kecil, Harun al-Rasyid telah memiliki sifat istimewa seperti pemberani dan kuat. Sifat ini menjadikannya sangat layak sebagai suksesor ayahnya saat ia berusia 20-an tahun.
+
+Menjabat Khalifah
+
+Harun al-Rasyid menjabat khalifah Daulah Abbasiyah menggantikan ayahnya, al-Mahdi. Pengangkatannya terjadi pada malam sabtu tanggal 16 Rabiul Awal 170 H. Jabatan tertinggi di Daulah Abbasiyah itu ia duduki hingga bulan Jumadil Akhir 194 H.
+
+Saat menjabat khalifah, umurnya baru menginjak 25 tahun. Ia berkun-yah dengan Abu Musa, namun orang-orang mengkun-yahinya dengan Abu Ja’far.
+
+Khalifah Yang Shaleh
+
+Al-Khatib al-Baghdadi menyebutkan dalam Tarikh Baghdad, “Sebagian sahabat Harun bercerita bahwa ia shalat setiap hari sebanyak 100 rakaat. Hal itu ia lakukan dengan istiqomah hingga wafat. Kecuali ada sebab yang menghalanginya. Ia bersedekah dengan mendermakan 1000 dirham setiap hari. Apabila ia menunaikan haji, turut serta bersamanya 100 ahli fikih (ulama) dan anak-anak mereka. Jika ia tidak berhaji, maka ia menghajikan 300 orang dengan bekal baju besi, kiswah, dan yang lainnya.” (Tarikh Baghdad Bab al-Ha-u)
+
+Al-Mas’udi mencatat tahun-tahun dimana Harun al-Rasyid menunaikan ibadah haji. Dari catatannya Harun al-Rasyid berhaji pada tahun 170, 173, 174, 175, 176, 177, 178, 179, 181, 186, dan 188 H.
+
+Adz-Dzahabi mengatakan dalam Tarikhnya, “Tahun 179, Harun al-Rasyid berumrah di bulan Ramadhan. Ia senantiasa dalam ihramnya hingga musim haji tiba. Ia berjalan dari rumahnya menuju Arafah.” (Siyar A’lam Nubala, Juz: 7 al-Rasyid).
+
+Amalan Harun al-Rasyid ini membantah orang-orang yang tidak berhaji dengan alasan peduli sosial. Kedua ibadah ini bisa dilakukan tanpa mengorbankan salah satunya. Harun al-Rasyid berhaji dan juga memiliki perhatian besar dalam hubungan sosial kemasyarakatan.
+
+Kehidupannya Adalah Teladan
+
+Al-Manshur bin Ammar mengatakan, “Aku tidak melihat orang yang lebih mudah menitikkan air mata saat berdzikir melebihi tiga orang: al-Fudhail bin Iyad, (Harun) al-Rasyid, dan yang lain.” (Mukhtashar Tarikh Dimasyq, Juz: 27, Hal: 19).
+
+Diriwayatkan, suatu hari Ibnu as-Samak menemui al-Rasyid. Saat itu Harun al-Rasyid meminta minum. Diberikanlah untuknya semangkok minuman. Ibnu as-Samak berkata, “Wahai Amirul Mukminin, seandainya Anda terhalangi meminum minuman ini –maksudnya satu mangkuk air ini pun Anda tak punya-, dengan apa Anda akan membelinya?” “Dengan setengah kerajaanku,” jawab al-Rasyid. “Minumlah, semoga Allah memberimu ketenangan,” kata Ibnu as-Samak.
+
+Setelah al-Rasyid selesai meminum air itu, Ibnu as-Samak kembali berkata, “Seandainya air ini dihalangi keluar dari badan Anda, dengan apa Anda akan menebusnya agar ia bisa keluar?” “Dengan seluruh wilayah kerajaanku,” jawab al-Rasyid. Ibnu as-Samak melanjutkan, “Sesungguhnya harga sebuah kerajaan hanya dengan seteguk air dan kencingnya. Sungguh tidak pantas seorang berlomba-lomba memperebutkannya.” Harun al-Rasyid pun menangis tersedu-sedu. (Tarikh al-Khulafa, Juz: 1, Hal: 216).
+
+Ibnul Jauzi mengisahkan, “al-Rasyid berkata kepada Syibyan, ‘Nashiatilah aku’. Syibyan mengatakan, “Bertemanlah dengan orang-orang yang membuatmu takut, tapi dengan itu engkau merasa aman. Hal ini lebih baik bagimu daripada berteman dengan orang yang membuatmu merasa aman, tapi engkau menjadi ketakutan.”
+
+“Jelaskan maksud ucapan itu padaku,” kata al-Rasyid.
+
+“Orang yang mengatakan padamu, ‘Engkau bertanggung jawab terhadap rakyatmu, maka takutlah kepada Allah’. Orang yang demikian lebih baik untukmu. Daripada mereka yang mengatakan, ‘Engkau adalah ahlul bait (keluarga rasul). Dosa-dosamu diampuni. Anda adalah kerabatnya Nabi ﷺ’.” Harun al-Rasyid pun menangis, sampai-sampai orang di sekelilingnya merasa kasihan padanya (Tarikh al-Khulafa, Juz: 1, Hal: 216).
+
+Mencintai Sunnah dan Para Ulama
+
+Al-Rasyid adalah seorang pemimpin yang cinta pada para ulama. Ia mengagungkan dan memuliakan agama. Membenci debat dan banyak bicara. Al-Qadhi al-Fadhil dalam sebagian suratnya mengatakan, “Aku tidak tahu ada seorang Raja yang tidak pernah beristirahat menuntut ilmu, kecuali al-Rasyid. Ia pergi bersama dua orang putranya, al-Amin dan al-Makmun, untuk mendengar al-Muwatha dibacakan oleh Imam Malik rahimahullah.” (Tarikh al-Khulafa, Juz: 1, Hal: 217)
+
+Saat sampai kabar padanya seorang tokoh tabiut tabi’in, Abdullah bin al-Mubarak rahimahullah, wafat, Harun al-Rasyid duduk bersedih. Dan para tokoh pun berusaha menghiburnya.
+
+Abu Muawiyah adh-Dharir mengatakan, “Tidaklah aku menyebut Nabi ﷺ di hadapan al-Rasyid kecuali ia mengatakan shallallahu ‘ala sayyidi (shalawat Allah atas tuanku). Kemudian kuriwayatkan kepadanya hadits beliau,
+
+وددت أني أقاتل في سبيل الله فأقتل ثم أحيى ثم أقتل
+
+“Andai saja aku berperang di jalan Allah, kemudian terbunuh. Setelah itu aku hidup kembali dan terbunuh kembali.” (HR. al-Bukhari 6799).
+
+Ia menagis hingga terisak-isak.
+
+Dari Khurzadz al-Abid, ia berkata, “Abu Muawiyah menyampaikan sebuah hadits –Nabi ﷺ- kepada al-Rasyid. Yakni hadits tentang kisah Nabi Adam mengalahkan hujjah Nabi Musa. Lalu ada seseorang yang bertanya, “Dimana keduanya bertemu?” al-Rasyid pun marah dan berkata, “Hamparkan kulit dan cabut pedang. Seorang zindiq telah menghina hadits”. Abu Muawiyah pun menenagkan Harun al-Rasyid hingga padam amarahnya. (al-Fawa-id adz-Dzahabiyah min Siyar A’lam Nubala, Juz: 1, Hal: 10)
+
+Hadits tersebut adalah:
+
+عَنْ طَاوُسٍ قَالَ سَمِعْتُ أَبَا هُرَيْرَةَ يَقُولُا قَالَ رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ احْتَجَّ آدَمُ وَمُوسَى فَقَالَ مُوسَى يَا آدَمُ أَنْتَ أَبُونَا خَيَّبْتَنَا وَأَخْرَجْتَنَا مِنْ الْجَنَّةِ فَقَالَ لَهُ آدَمُ أَنْتَ مُوسَى اصْطَفَاكَ اللَّهُ بِكَلَامِهِ وَخَطَّ لَكَ بِيَدِهِ أَتَلُومُنِي عَلَى أَمْرٍ قَدَّرَهُ اللَّهُ عَلَيَّ قَبْلَ أَنْ يَخْلُقَنِي بِأَرْبَعِينَ سَنَةً فَقَالَ النَّبِيُّ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ فَحَجَّ آدَمُ مُوسَى فَحَجَّ آدَمُ مُوسَى
+
+Adam dan Musa ‘alaihimasslam saling berdebat. Musa berkata, “Wahai Adam, engkau adalah bapak kami. Engkau telah mengecewakan kami dan mengeluarkan kami dari surga karena dosamu.”
+
+Adam menjawab, “Wahai Musa, Allah telah memilihmu dengan kalam-Nya dan menulis Taurat untukmu dengan tangan-Nya. Apakah kau mencelaku atas perkara yang telah Allah tentukan terhadapku empat puluh tahun sebelum Dia menciptakanku?”
+
+Nabi ﷺ bersabda, “Argumentasi Adam mengalahkan Musa. Argumentasi Adam mengalahkan Musa.” (HR. Muslim No.4793).
+
+Mendengar hadits ini, spontan salah seorang di majelis Harun al-Rasyid berseloroh, “Dimana keduanya bertemu?” Namun Harun al-Rasyid menangkap ucapan ini sebuah respon untuk membantah. Sehingga ia langsung merespon serius seseorang yang dianggapnya berani mendustakan hadits Nabi ﷺ. An-nuth’u wa as-saif (pedang), kata al-Rasyid. An-nuth’u adalah kulit yang dihamparkan untuk mengeksekusi seseorang agar darahnya tidak membasahi lantai.
+
+Mendustakan hadits bukan perkara kecil di mata Harun al-Rasyid. Ia sampai menyebut orang tersebut dengan zindiq, yakni orang yang mendustakan dan membantah syariat. Sementara kaum muslimin pada hari ini dengan mudah menolak hadits, tanpa merasa bersalah sedikit pun. Mereka mengatakan, “Hadits ini tidak lagi relevan dengan zaman sekarang”, “Hadits ini tafsirannya demikian dan demikian –bermaksud menolak hadits-”, dll.
+
+Al-Fudhail bin Iyadh rahimahullah mengatakan, “Tidak seorang pun meninggal, lebih berat terasa kematiannya dibandingkan Amirul Mukminin Harun. Aku berandai-andai sekiranya Allah menambahkan umurnya dari umurku.” Ia melanjutkan, “Berat terasa bagi kami. Saat Harun wafat, muncullah fitnah. Al-Makmun (putra Harun) menyeru masyarakat meyakini bahwa Alquran itu makhluk.” (at-Tafsir min Sunan Said bin Manshur, Hal: 25)
+
+Harun al-Rasyid dan Kaisar Romawi Nikephoros I
+
+Pada tahun 187 H, Harun al-Rasyid menerima surat dari Kaisar Romawi Nikephoros I. Surat tersebut berisi pembatalan perjanjian damai antara Romawi dan Abbasiyah yang telah disepakati oleh Kiasar Romawi sebelumnya. Isi surat Nikephoros adalah sebagai berikut:
+
+Dari Nikephoros, Kaisar Romawi, kepada Harun, Raja Arab. Amma ba’du..
+
+Sesungguhnya kaisar sebelumku memberimu posisi benteng (dalam permainan catur pen.). Dan dia memposisikan diri sebagai pion. Ia bawakan kepadamu harta-hartanya. Sebenarnya aku bisa memberikan jumlah berkali lipat darinya. Tapi itu karena kelemahannya dan kebodohannya sebagai seorang wanita. Jika engkau membaca suratku ini, kembalikan apa yang telah engkau dapatkan sebelumku! Jika tidak, maka pedang (yang berbicara) antara aku dan dirimu!
+
+Ketika al-Rasyid membaca surat ini, ia pun marah besar. Tidak ada seorang pun yang berani mengarahkan pandangan ke arah wajahnya. Apalagi mengeluarkan sepatah kata padanya. Orang-orang yang duduk bersamanya menyingkir karena takut. Menteri pun membisu. Al-Rasyid menulis surat balasan:
+
+Bismillahirrahmanirrahim.
+
+Dari Harun Amirul Mukminin, kepada Nikephoros anjing Romawi. Aku telah membaca suratmu wahai anak perempuan kafir. Jawabannya adalah sesuatu yang akan engkau lihat sebelum kau dengar.
+
+Hari itu juga Harun al-Rasyid memimpin sendiri pasukannya menuju Romawi. Sampai akhirnya al-Rasyid berhasil menaklukkan Kota Hercules –sebuah kota dekat Konstantinopel-, Nikephoros ketakutan. Ia kembali meminta perjanjian damai dan bersedia membayar upeti (Tarikh ath-Thabari bab Sanah Sab’u wa Tsamanin wa Mi-ah).
+
+Kemakmuran di Era Pemerintahannya
+
+Ada sepenggal kalimat yang diucapkan Harun al-Rasyid menggambarkan betapa luas dan makmur wilayah kekuasaannya. Suatu hari al-Rasyid melihat awan mendung, kemudia ia mengatakan,
+
+أمطري حيث شئت؛ فسيأتيني خراجك
+
+“Hujanlah dimanapun yang kau inginkan. Hasil bumi pun akan datang padaku.” (Mausu’ah Akhlak wa Zuhd wa Raqa-iq Juz 1 Hal: 198).
+
+Hujan tersebut akan bermanfaat bagi kaum muslimin, baik turun di wilayah kekuasaan Islam Dintasti Abbasiyah atau di luar wilayah tersebut. Jika dia turun di wilayah Islam, kaum muslimin akan memanfaatkan airnya untuk minum dan mengairi ladang mereka. Dan jika turun di selain wilayah kaum muslimin, hasil buminya akan datang kepada umat Islam dalam bentuk jizyah.
+
+Inilah gambaran kemuliaan, kemakmuran, dan kekuasaan kaum muslimin di era Harun al-Rasyid rahimahullah.
+
+Wafatnya al-Rasyid
+
+Harun al-Rasyid pernah bermimpi tentang kematiannya. Dalam mimpinya ia melihat dirinya menggenggam tanah berwarna merah. Di tempat itulah ia wafat.
+
+Mimpi itu pun jadi kenyataan. Saat al-Rasyid menempuh perjalanan menuju Khurasan, setibanya di Kota Thous, ia jatuh sakit. Al-Rasyid memerintahkan pembantunya, “Datangkan padaku sewadah tanah dari tempat ini.” Kemudian diberikan padanya tanah merah di gengagamannya. Melihat itu, al-Rasyid mengatakan, “Demi Allah, inilah telapak tangan yang aku lihat. Dan tanah yang ada di genggamannya.”
+
+Ia memerintahkan penggalian makamnya saat ia masih hidup. Kemudian ia minta dibacakan Alquran seutuhnya. Setelah itu, ia minta dibawa ke makamnya. “Menuju tempat inilah perjalanan (hidup ini) wahai anak Adam,” kata al-Rasyid. Ia pun menangis. Tiga hari kemudian, beliau rahimahullah wafat.
+
+
+
+Read more https://kisahmuslim.com/5656-harun-al-rasyid-khalifah-terbaik-dinasti-abbasiyah.html'''),
+              SizedBox(
+                height: 42,
+              ),
+              Wrap(
+                alignment: WrapAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Container(
+                      color: Color.fromRGBO(0, 0, 0, 0.04),
+                      padding: EdgeInsets.only(
+                          right: 12, left: 12, top: 8, bottom: 8),
+                      child: Text(
+                        "nahlmb.dev - 2020 - IDN Final Task\nsumber : kisahmuslim.com",
+                        style: TextStyle(color: Colors.black54),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 42,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
